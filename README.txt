0fbda7e5dbba60fa000a41692fb1335a8d1df428
nickimberlin <nickimberlin@gmail.com>
2024-02-08 18:33:36 -0600

    live camera updates

---
+++ README.txt
@@ -0,0 +1,2 @@
+- Create a Python Environment
+python -m venv myenv
--- server-websocket/main.py
+++ server-websocket/main.py
@@ -5,8 +5,10 @@ We are using a small REST server to control our robot.
 from flask import Flask, render_template_string, render_template, request, Response
 from flask_socketio import SocketIO, send, emit
 from flask_cors import CORS
-# from robot import forward_backward, left_right, stop
-import cv2
+
+# importing Camera and Robot controls
+from camera import gen
+from robot import forward_backward, left_right, stop
 
 _debug = False
 
@@ -14,17 +16,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 CORS(app)
 socketio = SocketIO(app)
-
-def gen():
-    cap = cv2.VideoCapture(0)  # 0 for the default camera
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -40,19 +31,19 @@ def default_error_handler(e):
 def control(message):
     data = message["data"]
     print("Control: ", data)
-    # stop()
+    stop()
 
 @socketio.on('joystick1', namespace='/joystick1')
 def control(message):
     data = message
     print("joystick1: ", data['data']['y'])
-    # forward_backward(data['data']['y'])
+    forward_backward(data['data']['y'])
 
 @socketio.on('joystick2', namespace='/joystick2')
 def control(message):
     data = message
     print("joystick2: ", data['data']['x'])
-    # left_right(data['data']['x'])
+    left_right(data['data']['x'])
 
 @app.route('/video_feed')
 def video_feed():
@@ -60,4 +51,3 @@ def video_feed():
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", debug=True, use_reloader=False)
-    print("Server is running")
