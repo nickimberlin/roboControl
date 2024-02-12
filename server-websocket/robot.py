f72e78832e783a51d0c0531892988a110a1ac334
nickimberlin <nickimberlin@gmail.com>
2024-02-12 14:13:00 -0600

    Joystick Print Logs Commented

--- server-websocket/robot.py
+++ server-websocket/robot.py
@@ -1,18 +1,18 @@
 import time
-# import board
-# from adafruit_motorkit import MotorKit
+import board
+from adafruit_motorkit import MotorKit
 
-# kit = MotorKit(i2c=board.I2C())
+kit = MotorKit(i2c=board.I2C())
 
 # Move the car forward or backward
 def forward_backward(data):
-    # kit.motor2.throttle = data
-    print( 'forward_backward >', data)
+    kit.motor2.throttle = data
+    # print( 'forward_backward >', data)
 
 # Turn the car left or right
 def left_right(data):
-    # kit.motor1.throttle = data
-    print( 'left_right >', data)
+    kit.motor1.throttle = data
+    # print( 'left_right >', data)
 
 # # Control Both Motors
 # def control_motors(data):
