a6963f0aafbbdb12b9a480734dee223f90fce9e9
nickimberlin <nickimberlin@gmail.com>
2024-02-11 23:20:53 -0600

    updates

--- server-websocket/camera.py
+++ server-websocket/camera.py
@@ -1,17 +1,35 @@
 import cv2
 import os
+from imutils.video import VideoStream
 
-def gen():
-    cap = cv2.VideoCapture(0)  # 0 for the default camera
+# def gen():
+#     # cv2 videoStream
+#     cap = cv2.VideoCapture(0)  # 0 for the default camera
+
+#     # Create a directory to save the frames even though it is not used
+#     if not os.path.exists('photos'):
+#         os.makedirs('photos')
+
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+#         ret, buffer = cv2.imencode('.jpg', frame)
+#         frame = buffer.tobytes()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
+#     cap.release()
+
+def videoStream():
+    vs = VideoStream(usePiCamera=False).start()
 
     # Create a directory to save the frames even though it is not used
     if not os.path.exists('photos'):
         os.makedirs('photos')
 
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+        frame = vs.read()
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
--- server-websocket/main.py
+++ server-websocket/main.py
@@ -1,14 +1,10 @@
-#!/usr/bin/python
-"""
-We are using a small REST server to control our robot.
-"""
 from flask import Flask, render_template_string, render_template, request, Response
 from flask_socketio import SocketIO, send, emit
 from flask_cors import CORS
 
 # importing Camera and Robot controls
-from camera import gen
-from robot import forward_backward, left_right, stop
+from camera import videoStream
+from robot import control_motors
 
 _debug = False
 
@@ -26,28 +22,24 @@ def default_error_handler(e):
     print("======================= ERROR")
     print(request.event["message"])
     print(request.event["args"])
-
-@socketio.on('control', namespace='/control')
-def control(message):
-    data = message["data"]
-    print("Control: ", data)
-    stop()
+    print("======================= ERROR")
 
 @socketio.on('joystick1', namespace='/joystick1')
 def control(message):
     data = message
-    print("joystick1: ", data['data']['y'])
-    forward_backward(data['data']['y'])
+    print(data)
+    control_motors(data['data'])
+    # forward_backward(data['data']['y'])
 
-@socketio.on('joystick2', namespace='/joystick2')
-def control(message):
-    data = message
-    print("joystick2: ", data['data']['x'])
-    left_right(data['data']['x'])
+# @socketio.on('joystick2', namespace='/joystick2')
+# def control(message):
+#     data = message
+#     left_right(data['data']['x'])
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(videoStream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", debug=True, use_reloader=False)
